72f0375a447bb190a812cc71dfde2ec6c42f4f61
Kumar Abhijeet <ab.progr@gmail.com>
2023-05-01 12:30:43 +0530

    js comment

---
+++ 01-variables-data-types/02-comments/script.js
@@ -0,0 +1,20 @@
+console.clear();
+
+console.log('Hello from comments')
+
+// comments are useful for:
+// documenting code
+// disabling code
+// writing down TODOs
+
+// single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+// More on comments can be read at:
+// https://www.w3schools.com/js/js_comments.asp
+
