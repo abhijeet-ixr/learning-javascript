67684734515e644bc398a03fc8a3f3247c1f1087
Kumar Abhijeet <ab.progr@gmail.com>
2023-05-02 12:38:28 +0530

    js math object

---
+++ 01-variables-data-types/11-math-object/script.js
@@ -0,0 +1,22 @@
+console.clear();
+console.log("Math Object");
+
+// Math is built in object that exposes mathematical properties and functions
+
+console.log(Math.sqrt(9));
+console.log(Math.abs(-9));
+
+console.log(Math.round(9.9));
+console.log(Math.ceil(9.9));
+console.log(Math.floor(9.9));
+
+console.log(Math.pow(9, 2));
+
+console.log(Math.min(9, 2, 10, 1));
+console.log(Math.max(9, 2, 10, 1));
+
+console.log(Math.random());
+
+// More readings:
+// https://www.w3schools.com/js/js_math.asp
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
