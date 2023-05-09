344d28ae420bedd1f0000b9560770a3e249932ef
Kumar Abhijeet <ab.progr@gmail.com>
2023-05-09 13:06:30 +0530

    js array methods

---
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -0,0 +1,42 @@
+console.clear();
+console.log("Array Methods");
+
+// array literal
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.push(6));
+console.log(arr);
+
+console.log(arr.pop());
+console.log(arr);
+
+console.log(arr.unshift(0));
+console.log(arr);
+
+console.log(arr.shift());
+console.log(arr);
+
+console.log(arr.reverse());
+console.log(arr);
+
+console.log(arr.includes(7));
+console.log(arr);
+
+console.log(arr.indexOf(2));
+console.log(arr);
+
+console.log(arr.slice(2));
+console.log(arr);
+
+console.log(arr.slice(2, 4));
+console.log(arr);
+
+console.log(arr.splice(2));
+console.log(arr);
+
+console.log(arr.splice(0, 1));
+console.log(arr);
+
+// More on console can be read at:
+// https://www.w3schools.com/js/js_arrays.asp
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
